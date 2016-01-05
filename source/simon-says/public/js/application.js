d43ab5d484d1a4b9bc0ff9ad341e4b37bc7bcd89
Mia Lehrer & GregoryW <pair+mllehrer.mryakamoto@devbootcamp.com>
2016-01-05 14:44:01 -0800

    finish simple simon says

--- source/simon-says/public/js/application.js
+++ source/simon-says/public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 
+  $( "#get_color" ).click(function(event){
+    event.preventDefault();
+    $.post('/color').done(function(string){
+      var cell = parseInt(string[0]);
+      var color = string.slice(1);
+      $( "ul li:nth-child(" + cell + ")" ).css("background-color", color);
+    });
+  });
+
 });
